6495fa42ffcacf4f9b4755036d600136b5aed745
Maryam Raozaman <159222566@aston.ac.uk>
2023-01-26 17:14:17 +0000

    created new flask project

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 @app.route("/")
-
 def say_hello():
-    return "<h1>Hello, Flask! </h1>"
+    return  render_template('home.html')
+
+
--- Presentations/flaskpresentations.py
+++ Presentations/flaskpresentations.py
@@ -3,3 +3,36 @@
 
 #flask is a web framework for python
 #choose a location, open new folder called flask and file called app.py
+
+#REST - creating an API
+#add an API
+    #client server architecture
+    #statelesness
+    #cacheability
+    #layered system
+    #uniform interface
+        #providing a unifrm interace simplfies application architecture, and decouples components of the system
+        #if our API is trult RESTful, a client shouldnt have to know anything about how an API is implemented in order to start making requests and utilizing the responses
+        #Richardson maturity level
+            #describes four levels of conformity with REST Principles
+            #should be aiming for level 2/3
+            #1 THE SWAMP OF POX, 2 RESOURCES, 3 HTTP VERBS,4 HYPERMEDIA CONTROLS
+            #Resources
+                #are the entities, be they documents, files, database records or other bits of data, that your application works with
+            #HTTP Verbs
+                #http request is made using a speciifc "method" and each method is expected to produce different behavior 
+                #commonly used HTTP methods include GET, POST, PATCH and DELETE. We can map these methods directly to the CRUD(create, read, update and delete) operations
+            #Hypermedia Controls(DONT NEED, BUT GOOD STRETCH TARGET)
+                #HATEOAS links - highest level of conformity to rest principles, an API should use hypermedia controls
+#Flask dependencies
+    #flask: the core flask library
+    #mysqlclient: a database driver for MYSQL server
+    #flask-SQLAlchemy: an interface between MYSQL and Python
+    #flask-marshmallow: an object serialiser; and 
+    #marshmallow - sqlalchemy: integration for the above libraries
+
+#Project - think about something to create and create an api based on this
+#modular programs are generally easier to read, test
+
+#flask configuration
+
